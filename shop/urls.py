52f00a18e30a3408451ed952e70e7b9ed4f885f9
Nikolai <Nikolai@MacBook-Pro.local>
2021-01-19 14:47:32 +0300

    add template for cart

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name ='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('cart', views.cart, name='cart'),
+
+]
